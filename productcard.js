e7fa553d750b4dbf1bebbae4711a149a41c61d98
mprestage <mprestage@student.unimelb.edu.au>
2022-05-26 15:32:38 +1000

    layout fixes

--- productcard.js
+++ productcard.js
@@ -3,8 +3,6 @@ productcard = document.getElementById("targetcard");
 function toggleExpand() {
     console.log("working")
     productcard.classList.toggle("expand")
-
-    
-    }
+}
 
 document.getElementById("targetcard").onclick = toggleExpand
